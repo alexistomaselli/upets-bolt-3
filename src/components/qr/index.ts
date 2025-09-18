bf5a418d5ba1cfc2e6b1320ac8d1b0ccc39ad5b1
alexistomaselli <alexistomaselli@users.noreply.github.com>
2025-09-18 19:25:34 -0300

    Fix QRScanner import error

--- src/components/qr/index.ts
+++ src/components/qr/index.ts
@@ -2,5 +2,4 @@
 export { QRList } from './QRList';
 export { QRCard } from './QRCard';
 export { QRManagement } from './QRManagement';
-export { QRScanner } from './QRScanner';
 export { QRStats } from './QRStats';
